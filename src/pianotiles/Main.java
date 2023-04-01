693b5b940fa65cbd2d20e905976c216bbe211ede
Combsss <combinatrocics@gmail.com>
2023-03-31 23:50:46 -0400

    some stuff

--- src/pianotiles/Main.java
+++ src/pianotiles/Main.java
@@ -1,22 +1,50 @@
 package pianotiles;
+
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.awt.*;
 import javax.swing.*;
 import java.io.*;
 import java.awt.event.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
+  
 
 public class Main extends JPanel implements MouseListener{
 	
+	//Utility
+	public static Graphics pencil;
+	
+	//Mouse pos
 	public static int x, y;
+	
+	//Screen
+	Dimension temp1 = Toolkit.getDefaultToolkit().getScreenSize();
+	public int X = (int)temp1.getWidth(), Y = (int)temp1.getHeight();
+	
+	
+	//Start button
+
+	public static BufferedImage but;
 	
 	
 	public Main() {
 		//Window settings
-		setPreferredSize(new Dimension(300, 300));
+		setPreferredSize(new Dimension(X, Y));
 		setBackground(new Color(255, 255, 255));
 		addMouseListener(this);
+		
+	}
+	
+	public void paint(Graphics pencil) {
+		try {
+			but = ImageIO.read(new File("startbutton.jpg"));
+			super.paintComponent(pencil);
+			pencil.drawImage(but, X/2-280, Y/2-180, null);
+		}catch (Exception e) {
+			System.out.println("FAIL");
+		}
 	}
 	
 	public static void main(String[] args) {
@@ -29,28 +57,41 @@ public class Main extends JPanel implements MouseListener{
 		frame.pack();
 		frame.setVisible(true);
 		//
-		
+		Main phone = new Main();
+		phone.startbutton(pencil);
 		
 		
 	}
 	
-	public void addkey(Graphics g) {
-		super.paintComponent(g);
-		keys hi = new keys();
-		g.drawOval(x-40, y-40, 80, 80);
-		hi.make_key(x, y, g);
+	
+	public void rain() {
 		
 	}
 	
-	//Extra
+	public void startbutton(Graphics pencil) {
+	
+		try {
+			but = ImageIO.read(new File("startbutton.jpg"));
+			super.paintComponent(pencil);
+			pencil.drawImage(but, X/2-280, Y/2-180, null);
+		}catch (Exception e) {
+			System.out.println("FAIL");
+		}
+		
+		
+	}
+	
+	//MOUSE stuff
 
 	public void mousePressed(MouseEvent e)
 	{
+		
+		
 		x = e.getX();
 		y = e.getY();
 		System.out.println(x);
 		System.out.println(y);
-		addkey(this.getGraphics());
+		
 	}
 	public void mouseClicked(MouseEvent e)
 	{	
@@ -65,3 +106,10 @@ public class Main extends JPanel implements MouseListener{
 	{
 	}
 }
+
+
+
+
+
+
+
